e94ede9444fbf168e07ea84a55dfb5e3acd5c6a9
Basit Pushoo <79221732+Basit715@users.noreply.github.com>
2025-11-27 13:38:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1643,7 +1643,7 @@ with tab15:
             "expenses": todays_expenses,
             "balance": new_balance
         }
-        df = df.concat(new_row, ignore_index=True)
+        df = pd.concat(new_row, ignore_index=True)
         save_payments(df)
     st.subheader("📘 Full Payment History")
     st.dataframe(df)
